a1af916c0fcdc91d99dbdb790002c65f7be2624b
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-11 01:14:09 +0200

    moving function definition

---
+++ functions.jl
@@ -0,0 +1,13 @@
+module Functions
+
+using Zygote
+using Statistics
+
+f_rosenbrock(x::Array{Float64})::Float64  = 100 * (x[2] - x[1]^2)^2 + (1 - x[1])^2
+∇f_rosenbrock(x::Array{Float64})::Array{Float64} = f_rosenbrock'(x)
+# a = 20, b = 0.2 and c = 2π.
+f_ackley(x::Array{Float64}, a=20.0,b=0.2,c=2.0 * pi)::Float64 = -a * exp(-b * sqrt(mean(x.^2.0))) - exp(mean(cos.(c .* x))) + a + exp(1.0)
+∇f_ackley(x::Array{Float64})::Array{Float64} = f_ackley'(x)
+
+export f_rosenbrock, ∇f_rosenbrock, f_ackley, ∇f_ackley
+end
