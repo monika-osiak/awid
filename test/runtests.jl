579983900074a0a08652ba0441877c175e44d06b
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-16 01:16:06 +0200

    Adding option to not always run performance tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 
 using Test
 
-include("./bench_methods.jl")
-include("./bench_functions.jl")
+benchmark_varialbe = "TEST_PERFORMANCE"
+include("./utils.jl")
+
+if haskey(ENV, benchmark_varialbe)
+    include("./bench_methods.jl")
+    include("./bench_functions.jl")
+end
